4db6daee575a1beaa5686f604618f7b00271a939
m2004mike <m56565566@gmail.com>
2017-07-14 15:32:50 +0800

    Add files via upload

---
+++ Myfirstclass.java
@@ -0,0 +1,11 @@
+package myfirst;
+
+public class Myfirstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello my First !");
+
+	}
+
+}
